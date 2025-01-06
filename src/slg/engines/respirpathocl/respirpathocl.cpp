5b48310c4cea79c8b63ec585a901944f74ca0ef9
Jason Qiu <jason.blank.qiu@gmail.com>
2025-01-06 11:03:06 +0800

    feat: implement render engine registry static methods in respir render engine

--- src/slg/engines/respirpathocl/respirpathocl.cpp
+++ src/slg/engines/respirpathocl/respirpathocl.cpp
@@ -19,3 +19,40 @@ PathOCLBaseOCLRenderThread* RespirPathOCLRenderEngine::CreateOCLThread(const u_i
 }
 
 #endif
+
+//------------------------------------------------------------------------------
+// Static methods used by RenderEngineRegistry
+//------------------------------------------------------------------------------
+
+Properties RespirPathOCLRenderEngine::ToProperties(const Properties &cfg) {
+	Properties props;
+
+	props <<
+			OCLRenderEngine::ToProperties(cfg) <<
+			cfg.Get(GetDefaultProps().Get("renderengine.type")) <<
+			PathTracer::ToProperties(cfg) <<
+			cfg.Get(GetDefaultProps().Get("pathocl.pixelatomics.enable")) <<
+			cfg.Get(GetDefaultProps().Get("opencl.task.count")) <<
+			Sampler::ToProperties(cfg) <<
+			PhotonGICache::ToProperties(cfg);
+
+	return props;
+}
+
+RenderEngine* RespirPathOCLRenderEngine::FromProperties(const RenderConfig *rcfg) {
+	return new PathOCLRenderEngine(rcfg);
+}
+
+const Properties &RespirPathOCLRenderEngine::GetDefaultProps() {
+	static Properties props = Properties() <<
+			OCLRenderEngine::GetDefaultProps() <<
+			Property("renderengine.type")(GetObjectTag()) <<
+			PathTracer::GetDefaultProps() <<
+			Property("pathocl.pixelatomics.enable")(true) <<
+			Property("opencl.task.count")("AUTO") <<
+			PhotonGICache::GetDefaultProps();
+
+	return props;
+}
+
+#endif
